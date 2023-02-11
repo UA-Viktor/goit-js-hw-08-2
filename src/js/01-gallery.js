926badd574a196cf71dde9b8519af63a314a85d6
UA-Viktor <v.korenkov@icloud.com>
2023-02-11 18:37:14 +0200

    update div

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -5,16 +5,14 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 const galleryEl = document.querySelector('.gallery');
 
 const cardsMarkup = galleryItems
-    .map(({preview,original,description}) => 
-        `<div class = "gallery__item">
-            <a class = "gallery__link" href = "${original}">
-                <img class = "gallery__image"
-                    src = "${preview}"
-                    data-source = "${original}"
-                    alt = "${description}"/>
-            </a><
-        /div>`)
-    .join("");
+    .map(({ preview, original, description }) => {
+        return `
+            <a class="gallery__item" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>
+        `;
+        })
+    .join('');
 
 galleryEl.insertAdjacentHTML('afterbegin', cardsMarkup);
 
